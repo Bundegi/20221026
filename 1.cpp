393029f786765266c7aff48302a2e87c6ac484e3
Yonguk Kim <yonguk99@naver.com>
2022-10-26 00:49:26 +0900

    first commit

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#define fast ios::sync_with_stdio(0), cin.tie(nullptr), cout.tie(nullptr)
+#define endl "\n"
+using namespace std;
+int Su[1000001] = { 0, };
+int main() {
+	fast;
+	int M, N;
+	cin >> M >> N;
+	Su[0] = Su[1] = 1;
+	for (int i = 2; i*i <= N; i++) {
+		if (Su[i] == 1) continue;
+		for (int j = i*i; j <= N; j += i) {
+			Su[j] = 1;
+		}
+	}
+	for (int i = M; i <= N; i++) if (Su[i] == 0) cout << i << endl;
+}
